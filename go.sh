217ae7e45aa4f9ae188639839f97f368c029e7ef
xavierpioche <xavier.pioche@laposte.net>
2023-03-26 21:06:38 -0400

    modification de go

--- go.sh
+++ go.sh
@@ -1,6 +1,11 @@
+# launch test script before compose
 WORKDIR=~/apps/split-screen-gstreamer-pod
+# cd ${WORKDIR}/streamer
+# docker build -t quad-streamer-gst .
+# cd ${WORKDIR}/web/
+# docker build -t split_screen_streamer .
 cd ${WORKDIR}/web/
 docker run -d -p 80:80 -v ${WORKDIR}/shared:/shared quad-streamer-web
 cd ${WORKDIR}/streamer
-docker run -d -p 8000:8000 -v ${WORKDIR}/shared:/shared quad-streamer-gst
+docker run -v ${WORKDIR}/shared:/shared quad-streamer-gst
 
